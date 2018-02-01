249878ff14b4eea48953352c4188058d35ffbcb9
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-02-01 13:53:25 -0500

    Add manipulating data instructions

--- materials/02_Manipulating_data/Exercise/Manipulating_Data.R
+++ materials/02_Manipulating_data/Exercise/Manipulating_Data.R
@@ -1,37 +1,29 @@
 # libraries ---------------------------------------------------------------
-library(tidyverse)
+# library(tidyverse)
 
+# 0. Download data -----------------------------------------------------------
+# 0.1. Create a new Rproj
+# 0.2. Create a folder "data/" to store your raw data
+# 0.3. Download the data from https://ndownloader.figshare.com/files/2292169
 
-# Download data -----------------------------------------------------------
-download.file("https://ndownloader.figshare.com/files/2292169",
-              "data/portal_data_joined.csv")
+# 1. Read data ---------------------------------------------------------------
+# 1.1 Read data using your prefer read function (e.g., read.csv, read_csv, read.table)
 
-# Read data -----------------------------------------------------------
-dat_mammals<-read.csv("./data/portal_data_joined.csv")
-head(dat_mammals)
-plot(dat_mammals$sex)
+# 2. Inspect data ------------------------------------------------------------
+# 2.1. What is the class of the data, and the variables of the data?
+# 2.2. What are the main dimensions of the data? (i.e., number of columns and rows)
+# 2.3. Check the first and last 6 rows of the data
+# 2.4. What are the main variables of the data? 
+# 2.5. Check the summary statistics for each column
 
-dat_mammals<-read_csv("./data/portal_data_joined.csv")
-plot(dat_mammals$sex)
+# 3. Factors in R ------------------------------------------------------------
+# 3.1. Convert the variables species_id, sex, genus into factors
 
-dat_mammals$sex<-as.factor(dat_mammals$sex)
-plot(dat_mammals$sex)
-
-
-# Inspecting data ------------------------------------------------------------
-# 1. summary
-
-
-# Manipulating data ------------------------------------------------------------
-summary(dat_mammals)
-
-# 1. Selecting columns and filtering rows
-# 2. Create new columns or variables
-# 3. Split-apply-combine data analysis
-# 4. summarize() function
-# 5. Tallying
-# 6. Reshaping data (gather and spread)
-# 7. Export data
-# 8. Pipes
-
-
+# 4. Manipulate data ------------------------------------------------------------
+# 4.1. Create a new object called "date" which includes the year, month and day columns
+# 4.2. Create a new object called survey2000s which excludes the data before the year 2000
+# 4.3. Create a new variable of weight with a natural log tranformation (i.e., log(weight))
+# 4.4. Check the number of species, plots and the median hindfoot length in the dataframe
+# 4.5. Compute the mean weight in kg by Genus
+# 4.6. Calculate the number of records per year
+# 4.7. Create a csv excluding the NAs of weight, and including only the species, genus, year and plot columns
