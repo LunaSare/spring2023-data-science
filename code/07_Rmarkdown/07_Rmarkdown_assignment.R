221dbe307e34cf344d4e9255e13a2b5363d5f7b2
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-02 12:32:22 -0500

    Include code for assignment

--- code/07_Rmarkdown/07_Rmarkdown_assignment.R
+++ code/07_Rmarkdown/07_Rmarkdown_assignment.R
@@ -1,6 +1,6 @@
 # Libraries ---------------------------------------------------------------
-
-
+library(knitr)
+library(tidyverse)
 
 # Data --------------------------------------------------------------------
 # Download data
@@ -15,9 +15,50 @@ Amniote<-read.csv("./data/Amniote_Database_Aug_2015.csv")
 
 # Explore data
 # View(Amniote)
+str(Amniote)
 # Note that Null values are listed as -999
 
 # Replace -999 for NAs
 Amniote[Amniote==-999]<-NA
 
+# Explore data ------------------------------------------------------------
+names(Amniote)
 
+## Species per class (In your rmarkdown, use the function kable to create a pretty table)
+Amniote %>% 
+  group_by(class) %>% 
+  summarise(N_order=n_distinct(order),
+            N_families=n_distinct(family),
+            N_species=n_distinct(species))
+
+
+# Relationship between body size and maximum longevity
+ggplot(Amniote, aes(x=maximum_longevity_y, y=log(adult_body_mass_g))) + 
+  geom_point()+
+  stat_smooth()
+
+# Ignoring whales and turtles
+Amniote %>% 
+  filter(order!="Cetacea"& order!="Testudines") %>% 
+  ggplot(aes(x=maximum_longevity_y, y=log(adult_body_mass_g))) + 
+  geom_point()+
+  stat_smooth()
+
+# Check the order levels of Mammals
+Amniote %>% 
+  filter(class=="Mammalia") %>% 
+  select(order) %>% 
+  unique
+
+Amniote %>% 
+  filter(order=="Primates" | order=="Cetacea") %>% 
+  ggplot(aes(x=maximum_longevity_y, y=log(adult_body_mass_g))) + 
+  geom_point(aes(colour=order))
+
+Amniote %>%
+  filter(order=="Cetacea" & maximum_longevity_y > 200) %>%
+  select(genus, species)
+
+Amniote %>%
+  filter(order=="Primates" & maximum_longevity_y > 70) %>%
+  select(genus, species)
--- readings/week7_reading.md
+++ readings/week7_reading.md
@@ -8,6 +8,5 @@ language: R
 ## Macroecology
 
 
-
 ## Ecoinformatics
 [Getting Started with R Markdown by Coding club](https://ourcodingclub.github.io/2016/11/24/rmarkdown-1.html#identify)
